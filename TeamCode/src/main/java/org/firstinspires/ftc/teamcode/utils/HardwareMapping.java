0ec5bcf0096433f6f9d1cee9b8937c80d9572f95
The-Bober <rares.dobrisan@gmail.com>
2024-07-30 19:28:36 +0300

    lift&mapping

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/HardwareMapping.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/HardwareMapping.java
@@ -14,16 +14,17 @@ public class HardwareMapping {
     public static final String BACK_RIGHT_MOTOR = "motorBR";
     public static final String INTAKE_MOTOR = "motorIntake";
     public static final String LIFT_MOTOR = "motorLift";
+    public static final String LIFT_MOTOR_2 = "motorLift2";  // New lift motor
     public static final String IMU = "imu";
     public static final String LServo = "servoL";
     public static final String RServo = "servoR";
 
     public final DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor, intakeMotor;
-    public final DcMotorEx motorLift;
+    public final DcMotorEx motorLift, motorLift2;  // Added motorLift2
     public final IMU imu;
     public final Servo ServoL, ServoR;
 
-    public HardwareMapping(DcMotorEx motorLift, DcMotor frontLeftMotor, DcMotor backLeftMotor, DcMotor frontRightMotor, DcMotor backRightMotor, DcMotor intakeMotor, IMU imu, Servo servoL, Servo servoR) {
+    public HardwareMapping(DcMotorEx motorLift, DcMotorEx motorLift2, DcMotor frontLeftMotor, DcMotor backLeftMotor, DcMotor frontRightMotor, DcMotor backRightMotor, DcMotor intakeMotor, IMU imu, Servo servoL, Servo servoR) {
         this.frontLeftMotor = frontLeftMotor;
         this.backLeftMotor = backLeftMotor;
         this.frontRightMotor = frontRightMotor;
@@ -31,11 +32,13 @@ public class HardwareMapping {
         this.intakeMotor = intakeMotor;
         this.imu = imu;
         this.motorLift = motorLift;
+        this.motorLift2 = motorLift2;  // Initialize the new motor
         this.ServoL = servoL;
         this.ServoR = servoR;
 
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorLift2.setDirection(DcMotorSimple.Direction.REVERSE);
 
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -43,6 +46,7 @@ public class HardwareMapping {
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLift2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -50,18 +54,19 @@ public class HardwareMapping {
         backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     public static HardwareMapping from(HardwareMap map) {
         return new HardwareMapping(
                 map.get(DcMotorEx.class, LIFT_MOTOR),
+                map.get(DcMotorEx.class, LIFT_MOTOR_2),
                 map.dcMotor.get(FRONT_LEFT_MOTOR),
                 map.dcMotor.get(BACK_LEFT_MOTOR),
                 map.dcMotor.get(FRONT_RIGHT_MOTOR),
                 map.dcMotor.get(BACK_RIGHT_MOTOR),
                 map.dcMotor.get(INTAKE_MOTOR),
-                map.get(IMU.class, IMU),// Corrected IMU retrieval
+                map.get(IMU.class, IMU),
                 map.servo.get(LServo),
                 map.servo.get(RServo)
         );
